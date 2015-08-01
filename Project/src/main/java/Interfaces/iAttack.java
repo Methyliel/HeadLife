384504d70f6747d91b568c0910dcb3f09c14cb58
Methyliel <littlesnoflake13@gmail.com>
2015-08-01 19:54:23 +0300

    Add new interface iAttack.

---
+++ Project/src/main/java/Interfaces/iAttack.java
@@ -0,0 +1,10 @@
+package Interfaces;
+
+public interface iAttack {
+    public void setHealthPoint(int healthPoint);
+    public int getHealthPoint();
+    public void setDamage(int damage);
+    public int getDamage();
+    public void setRange(int range);
+    public int getRange();
+}
